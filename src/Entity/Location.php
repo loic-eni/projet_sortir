2642f984823c6539ecc2f9c394ba737822fefab9
Loic DUVAIL <loic.duvail2022@campus-eni.fr>
2025-01-20 14:31:18 +0100

    add: create model

---
+++ src/Entity/Location.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $street = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $lat = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $long = null;
+
+    #[ORM\ManyToOne(inversedBy: 'locations')]
+    private ?city $city = null;
+
+    /**
+     * @var Collection<int, Outing>
+     */
+    #[ORM\OneToMany(targetEntity: Outing::class, mappedBy: 'location')]
+    private Collection $outings;
+
+    public function __construct()
+    {
+        $this->outings = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): static
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(?float $lat): static
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLong(): ?float
+    {
+        return $this->long;
+    }
+
+    public function setLong(?float $long): static
+    {
+        $this->long = $long;
+
+        return $this;
+    }
+
+    public function getCity(): ?city
+    {
+        return $this->city;
+    }
+
+    public function setCity(?city $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Outing>
+     */
+    public function getOutings(): Collection
+    {
+        return $this->outings;
+    }
+
+    public function addOuting(Outing $outing): static
+    {
+        if (!$this->outings->contains($outing)) {
+            $this->outings->add($outing);
+            $outing->setLocation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOuting(Outing $outing): static
+    {
+        if ($this->outings->removeElement($outing)) {
+            // set the owning side to null (unless already changed)
+            if ($outing->getLocation() === $this) {
+                $outing->setLocation(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Outing.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OutingRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OutingRepository::class)]
+class Outing
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $registrationMaxDate = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $maxInscriptions = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $outingInfo = null;
+
+    #[ORM\ManyToOne]
+    private ?state $state = null;
+
+    #[ORM\ManyToOne(inversedBy: 'outings')]
+    private ?location $location = null;
+
+    #[ORM\ManyToOne(inversedBy: 'outings')]
+    private ?campus $campus = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'outing')]
+    private Collection $participants;
+
+    #[ORM\ManyToOne(inversedBy: 'outings')]
+    private ?user $organizer = null;
+
+    public function __construct()
+    {
+        $this->participants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getRegistrationMaxDate(): ?\DateTimeInterface
+    {
+        return $this->registrationMaxDate;
+    }
+
+    public function setRegistrationMaxDate(\DateTimeInterface $registrationMaxDate): static
+    {
+        $this->registrationMaxDate = $registrationMaxDate;
+
+        return $this;
+    }
+
+    public function getMaxInscriptions(): ?int
+    {
+        return $this->maxInscriptions;
+    }
+
+    public function setMaxInscriptions(?int $maxInscriptions): static
+    {
+        $this->maxInscriptions = $maxInscriptions;
+
+        return $this;
+    }
+
+    public function getOutingInfo(): ?string
+    {
+        return $this->outingInfo;
+    }
+
+    public function setOutingInfo(?string $outingInfo): static
+    {
+        $this->outingInfo = $outingInfo;
+
+        return $this;
+    }
+
+    public function getState(): ?state
+    {
+        return $this->state;
+    }
+
+    public function setState(?state $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getLocation(): ?location
+    {
+        return $this->location;
+    }
+
+    public function setLocation(?location $location): static
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getCampus(): ?campus
+    {
+        return $this->campus;
+    }
+
+    public function setCampus(?campus $campus): static
+    {
+        $this->campus = $campus;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getParticipants(): Collection
+    {
+        return $this->participants;
+    }
+
+    public function addParticipant(User $participant): static
+    {
+        if (!$this->participants->contains($participant)) {
+            $this->participants->add($participant);
+            $participant->addOuting($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParticipant(User $participant): static
+    {
+        if ($this->participants->removeElement($participant)) {
+            $participant->removeOuting($this);
+        }
+
+        return $this;
+    }
+
+    public function getOrganizer(): ?user
+    {
+        return $this->organizer;
+    }
+
+    public function setOrganizer(?user $organizer): static
+    {
+        $this->organizer = $organizer;
+
+        return $this;
+    }
+}
