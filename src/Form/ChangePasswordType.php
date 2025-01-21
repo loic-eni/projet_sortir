b61720d0d8ab4923548e63a650dc336a89d7fc5e
Loic DUVAIL <loic.duvail2022@campus-eni.fr>
2025-01-21 11:00:32 +0100

    chore: remove useless imports

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -2,10 +2,6 @@
 
 namespace App\Form;
 
-use App\Entity\campus;
-use App\Entity\outing;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
