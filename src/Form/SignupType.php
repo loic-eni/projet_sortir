b7ad672d74baf9ee7dcf2289a7661abf3a93bb59
Yohan LAUNAY <yohan.launay2022@campus-eni.fr>
2025-01-22 10:24:42 +0100

    Style form register & login, add repeat password

--- src/Form/SignupType.php
+++ src/Form/SignupType.php
@@ -4,21 +4,31 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class SignupType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
-            ->add('firstname', null ,['label'=>'Prénom'])
-            ->add('lastname', null, ['label'=>'Nom'])
-            ->add('phone', null, ['label'=>'Téléphone'])
+            ->add('email', EmailType::class)
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer mot de passe'],
+            ])
+            ->add('firstname', TextType::class ,['label'=>'Prénom'])
+            ->add('lastname', TextType::class, ['label'=>'Nom'])
+            ->add('phone', TextType::class, ['label'=>'Téléphone'])
             ->add('save', SubmitType::class, ['label'=>'Créer le compte'])
         ;
     }
