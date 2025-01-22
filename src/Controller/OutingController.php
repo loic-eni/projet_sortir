1103ead4da4d13c6a6c2dff0f583f4f1534a5526
Yohan LAUNAY <yohan.launay2022@campus-eni.fr>
2025-01-22 16:22:02 +0100

    Register outing and start delete register

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Outing;
 use App\Form\OutingType;
 use App\Repository\OutingRepository;
+use App\Repository\StateRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +15,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/outing', name: 'outing_')]
-final class OutingController extends AbstractController
+final class OutingController extends BaseController
 {
     #[Route('/list', name: 'list', methods: ['GET'])]
     public function list(): Response
@@ -61,13 +63,74 @@ final class OutingController extends AbstractController
     {
         $outing = $outingRepository->find($id);
 
+        $participants = $outing->getParticipants();
+
         if(!$outing){
             throw $this->createNotFoundException("Outing not found");
         }
 
         return $this->render('outing/details.html.twig', [
                 'outing' => $outing,
+                'participants' => $participants,
+                'state' => self::STATE
             ]
         );
     }
+
+    #[Route('/register/new/{id}', name: 'register_new', methods: ['GET', 'POST'])]
+    public function new(EntityManagerInterface $entityManager, Outing $outing): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = $this->getUser();
+
+        if ($outing->getParticipants()->contains($user)) {
+            $this->addFlash('warning', 'Vous êtes déjà inscrit à cette sortie.');
+            return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
+        }
+
+        $status = $outing->getState()->getLabel();
+        if (!in_array($status, [self::STATE[0], self::STATE[1]]) ||
+            count($outing->getParticipants()) >= $outing->getMaxInscriptions() ||
+            $outing->getRegistrationMaxDate() < new \DateTime()) {
+            $this->addFlash('warning', 'Impossible de vous inscrire à cette sortie (soit le statut n\'est pas "Ouverte", soit le nombre d\'inscriptions est atteint ou soit date limite d\'inscription est dépassé)');
+            return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
+        }
+
+        $outing->addParticipant($user);
+        $entityManager->persist($outing);
+        $entityManager->flush();
+        $this->addFlash('success', 'Vous avez été inscrit à la sortie.');
+
+
+        return $this->redirectToRoute('outing_details', ['id' => $outing->getId()], Response::HTTP_SEE_OTHER);
+    }
+
+
+
+    #[Route('/register/remove/{id}', name: 'register_remove', methods: ['GET', 'POST'])]
+    public function remove(EntityManagerInterface $entityManager, Outing $outing): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = $this->getUser();
+
+        if ($outing->getParticipants()->contains($user)) {
+            $outing->removeParticipant($user);
+
+            $entityManager->persist($outing);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vous avez été retiré de la sortie.');
+        } else {
+            $this->addFlash('warning', 'Vous n\'êtes pas inscrit à cette sortie.');
+        }
+
+        return $this->redirectToRoute('outing_details', ['id' => $outing->getId()]);
+    }
+
 }
