6138386b2c0ab1b53ef8c204308d396e2c25803e
FleuryEdwin <edwin.fleury2024@campus-eni.fr>
2025-01-27 10:33:28 +0100

    Add user outing create form validation constraint

--- src/Form/OutingType.php
+++ src/Form/OutingType.php
@@ -5,12 +5,16 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\Location;
 use App\Entity\Outing;
-use App\Entity\State;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class OutingType extends AbstractType
 {
@@ -20,34 +24,86 @@ class OutingType extends AbstractType
             ->add('name', null, [
                 'label' => 'Nom: ',
                 'attr' => ['class' => 'formField'],
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un nom'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Le nom doit faire minimum 5 caractères',
+                        'max' => 255,
+                        'maxMessage' => 'Le nom ne peut pas dépasser 255 caractères'
+                    ]),
+                ]
             ])
             ->add('startDate', null, [
                 'widget' => 'single_text',
                 'label' => 'Date de début: ',
                 'attr' => ['class' => 'formField'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir la date de commencement de la sortie'
+                    ]),
+                    new GreaterThan([
+                        'propertyPath' => 'parent.all[registrationMaxDate].data',
+                        'message' => 'La date de début doit être superieur à la date d\'inscription'
+                    ]),
+                ]
             ])
             ->add('duration', IntegerType::class, [
                 'attr' => [
                     'min' => 1,
-                    'max' => 8,
+                    'max' => 480,
                     'step' => 1,
                     'class' => 'formField',
                 ],
-                'label' => 'Durée: '
+                'label' => 'Durée: ',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une durée'
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 480,
+                        'notInRangeMessage' => 'Le nombre d\'inscrit doit etre compris en 1 et 50'
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[0-9]+$/',
+                        'message' => 'Veuillez saisir un nombre'
+                    ])
+                ]
             ])
             ->add('registrationMaxDate', null, [
                 'widget' => 'single_text',
                 'label' => 'Date d\'inscription: ',
                 'attr' => ['class' => 'formField'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une date d\'inscription'
+                    ])
+                ]
             ])
             ->add('maxInscriptions', IntegerType::class, [
                 'attr' => [
-                    'min' => 0,
+                    'min' => 2,
                     'max' => 50,
                     'step' => 1,
                     'class' => 'formField',
                 ],
-                'label' => 'Nombre maximum d\'inscriptions: '
+                'label' => 'Nombre maximum d\'inscriptions: ',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un nombre'
+                    ]),
+                    new Range([
+                        'min' => 1,
+                        'max' => 50,
+                        'notInRangeMessage' => 'Le nombre d\'inscrit doit etre compris en 1 et 50'
+                    ])
+                ]
             ])
             ->add('outingInfo', null, [
                 'label' => 'Informations',
@@ -57,12 +113,14 @@ class OutingType extends AbstractType
                 'class' => location::class,
                 'choice_label' => 'name',
                 'label' => 'Localisation: ',
+                'required' => true,
                 'attr' => ['class' => 'formField'],
             ])
             ->add('campus', EntityType::class, [
                 'class' => campus::class,
                 'choice_label' => 'name',
                 'label' => 'Campus: ',
+                'required' => true,
                 'attr' => ['class' => 'formField'],
             ])
         ;
