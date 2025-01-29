58213bb4b2bcca7b27adc058432a9f00d9f2c34f
Yohan LAUNAY <yohan.launay2022@campus-eni.fr>
2025-01-29 14:23:52 +0100

    daisyui theme

--- assets/app.js
+++ assets/app.js
@@ -8,24 +8,19 @@ import './bootstrap.js';
 import './styles/app.css';
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Charger le thème enregistré depuis localStorage (ou 'light' par défaut)
     const savedTheme = localStorage.getItem('theme') || 'light';
 
-    // Appliquer le thème sélectionné à la racine du document
     document.documentElement.setAttribute('data-theme', savedTheme);
 
-    // Sélectionner l'élément <select> du thème
     const themeSelect = document.querySelector('select[name="theme"]');
 
-    // Si l'élément existe, mettre à jour sa valeur en fonction du thème enregistré
     if (themeSelect) {
-        themeSelect.value = savedTheme;  // Appliquer le thème dans le menu déroulant
+        themeSelect.value = savedTheme;
 
-        // Écouter le changement de sélection dans le <select>
         themeSelect.addEventListener('change', function () {
             const selectedTheme = themeSelect.value;
-            document.documentElement.setAttribute('data-theme', selectedTheme); // Appliquer le thème
-            localStorage.setItem('theme', selectedTheme); // Sauvegarder le thème dans localStorage
+            document.documentElement.setAttribute('data-theme', selectedTheme);
+            localStorage.setItem('theme', selectedTheme);
         });
     }
 });
